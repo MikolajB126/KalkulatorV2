8162e4b0efef95d33fb2b8be8d1bfdf54d835138
MikolajB126 <szkola1mikbar@gmail.com>
2025-09-17 12:07:45 +0200

    Update Operation.java

--- app/src/main/java/com/example/kalkulatorv2/Operation.java
+++ app/src/main/java/com/example/kalkulatorv2/Operation.java
@@ -1,28 +1,9 @@
 package com.example.kalkulatorv2;
 
 public enum Operation {
-    NONE(""),
-    ADD("+"),
-    SUBSTRACT("-"),
-    MULTIPLY("*"),
-    DIVIDE("/");
-
-    private final String key;
-
-    Operation(String key) {
-        this.key = key;
-    }
-
-    public String getKey() {
-        return key;
-    }
-
-    public static Operation operationFromKey(String key) {
-        for (Operation operation : values()) {
-            if (operation.key.equals(key)) {
-                return operation;
-            }
-        }
-        return NONE;
-    }
+    ADD,
+    SUBTRACT,
+    MULTIPLY,
+    DIVIDE,
+    NONE
 }
